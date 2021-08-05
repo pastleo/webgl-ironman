eb01fbe67e689b8b8222a9b870b0dc84635f5dfa
PastLeo <chgu82837@gmail.com>
2021-08-06 00:59:17 +0800

    01-hell-webgl: day 2

--- 01-hello-webgl.js
+++ 01-hello-webgl.js
@@ -4,3 +4,45 @@ window.gl = gl;
 
 gl.clearColor(108/255, 225/255, 153/255, 1);
 gl.clear(gl.COLOR_BUFFER_BIT);
+
+function createShader(gl, type, source) {
+  const shader = gl.createShader(type);
+  gl.shaderSource(shader, source);
+  gl.compileShader(shader);
+  const ok = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+  if (ok) return shader;
+
+  console.log(gl.getShaderInfoLog(shader));
+  gl.deleteShader(shader);
+}
+
+function createProgram(gl, vertexShader, fragmentShader) {
+  const program = gl.createProgram();
+  gl.attachShader(program, vertexShader);
+  gl.attachShader(program, fragmentShader);
+  gl.linkProgram(program);
+
+  const ok = gl.getProgramParameter(program, gl.LINK_STATUS);
+  if (ok) return program;
+
+  console.log(gl.getProgramInfoLog(program));
+  gl.deleteProgram(program);
+}
+
+const vertexShaderSource = `
+attribute vec2 a_position;
+ 
+void main() {
+  gl_Position = vec4(a_position, 0, 1);
+}
+`;
+ 
+const fragmentShaderSource = `
+void main() {
+  gl_FragColor = vec4(0.4745, 0.3333, 0.2823, 1);
+}
+`;
+
+const vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
+const fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
+const program = createProgram(gl, vertexShader, fragmentShader);
