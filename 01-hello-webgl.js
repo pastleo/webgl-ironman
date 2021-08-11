9d86080123c4cd53a2de6904f0aa70033433f1e1
PastLeo <chgu82837@gmail.com>
2021-08-11 23:59:40 +0800

    01-hello-webgl: day 5

--- 01-hello-webgl.js
+++ 01-hello-webgl.js
@@ -35,20 +35,28 @@ function createProgram(gl, vertexShader, fragmentShader) {
 
 const vertexShaderSource = `
 attribute vec2 a_position;
+attribute vec3 a_color;
 
 uniform vec2 u_resolution;
+
+varying vec3 v_color;
  
 void main() {
   gl_Position = vec4(
     a_position / u_resolution * vec2(2, -2) + vec2(-1, 1),
     0, 1
   );
+  v_color = a_color;
 }
 `;
  
 const fragmentShaderSource = `
+precision mediump float;
+
+varying vec3 v_color;
+
 void main() {
-  gl_FragColor = vec4(0.4745, 0.3333, 0.2823, 1);
+  gl_FragColor = vec4(v_color, 1);
 }
 `;
 
@@ -57,8 +65,10 @@ const fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource
 const program = createProgram(gl, vertexShader, fragmentShader);
 
 const positionAttributeLocation = gl.getAttribLocation(program, 'a_position');
+const colorAttributeLocation = gl.getAttribLocation(program, 'a_color');
 const resolutionUniformLocation = gl.getUniformLocation(program, 'u_resolution');
 
+// a_position
 const positionBuffer = gl.createBuffer();
 gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 
@@ -78,10 +88,50 @@ gl.bufferData(
     150, 60,
     180, 82.5,
     120, 82.5,
+
+    100, 60,
+    80, 40,
+    120, 40,
+
+    200, 60,
+    180, 40,
+    220, 40,
+  ]),
+  gl.STATIC_DRAW,
+);
+
+// a_color
+const colorBuffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+
+gl.enableVertexAttribArray(colorAttributeLocation);
+gl.vertexAttribPointer(
+  colorAttributeLocation,
+  3, // size
+  gl.UNSIGNED_BYTE, // type
+  true, // normalize
+  0, // stride
+  0, // offset
+);
+
+gl.bufferData(
+  gl.ARRAY_BUFFER,
+  new Uint8Array([
+    121, 85, 72,
+    121, 85, 72,
+    121, 85, 72,
+
+    0, 0, 0,
+    255, 255, 255,
+    255, 255, 255,
+
+    0, 0, 0,
+    255, 255, 255,
+    255, 255, 255,
   ]),
   gl.STATIC_DRAW,
 );
 
 gl.useProgram(program);
 gl.uniform2f(resolutionUniformLocation, canvas.width, canvas.height);
-gl.drawArrays(gl.TRIANGLES, 0, 3);
+gl.drawArrays(gl.TRIANGLES, 0, 9);
